67357747089f62332de040e0a49573911f59d527
Florian Behrens <florian.behrens@rocketmail.com>
2017-06-14 11:37:48 +0200

    Last try using keras-js

--- machine_learning/main.py
+++ machine_learning/main.py
@@ -5,7 +5,7 @@ import math
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
-filepath = "/home/ocius/Downloads/data_1496921703645.json"
+filepath = "/home/ocius/Downloads/data_1496923624387.json"
 fp = open(filepath)
 raw_data = json.load(fp)
 print("Training on sample set of size " + str(len(raw_data)))
@@ -25,9 +25,12 @@ prob_key_max = 0.95
 prob_key_min = 0.05
 
 def normalizeDataPoint(r):
+    """
     relative_wheel_angle = (r["wheel_angle"] + max_wheel_angle) / (2 * max_wheel_angle) # 0: all left, 0.5: straigt, 1: right
     relative_velocity = math.sqrt(r["velocity"]["x"] ** 2 + r["velocity"]["y"] ** 2) / max_speed # 0: stop, 1: full speed
     normalized = [relative_velocity, relative_wheel_angle]
+    """
+    normalized = []
     for sensorName in sorted(r["sensors"].keys()):
         sensor_range = long_range if sensorName in long_range_sensors else short_range
         sensor_value = r["sensors"][sensorName]
@@ -83,8 +86,8 @@ print(normalized_output[1])
 """
 
 model = Sequential()
-model.add(Dense(30, activation='relu', input_shape=(10,)))
-model.add(Dense(30, activation='relu'))
+model.add(Dense(40, activation='relu', input_shape=(8,)))
+model.add(Dense(24, activation='relu'))
 model.add(Dense(4, activation='softmax'))
 
 model.summary()
@@ -94,7 +97,7 @@ model.compile(loss='mean_squared_error',
               metrics=['mean_squared_error'])
 
 batch_size = 512
-epochs = 20
+epochs = 100
 
 history = model.fit(training_set_x, training_set_y,
                     batch_size=batch_size,
